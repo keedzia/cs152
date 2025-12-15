c622b38e1a66391f2c61f0170a135ce0926d12c7
keedzia <kar.kedzia@gmail.com>
2025-12-15 12:38:43 -0300

    first draft yay!!

---
+++ bars.py
@@ -0,0 +1,49 @@
+bars = {
+    'name': "Alvear Roof Bar",
+    'type': 'bar',
+    'budget': 'expensive',
+    'distance': 'walking',
+    'vibe': 'chill',
+    'reservation': 'yes',
+    'group_size': ['solo','couple', 'small_group', 'large_group'],
+    'wifi': 'yes',
+    'address': 'Av. Alvear 1891, C1129AAA Buenos Aires, BA',
+    'google_maps': 'https://www.google.com/maps/place/Av.+Alvear+1891,+C1129AAA+Buenos+Aires,+BA/data=!4m2!3m1!1s0x95bccaa37c9450ad:0x9cdf7e292ed4c101?sa=X&ved=1t:242&ictx=111'
+ 
+},
+{
+    'name': "On Tap Retiro",
+    'type': 'bar',
+    'budget': 'cheap',
+    'distance': 'walking',
+    'vibe': 'lively',
+    'reservation': 'yes',
+    'group_size': ['solo','couple', 'small_group'],
+    'wifi': 'yes',
+    'address': 'Marcelo Torcuato de Alvear 834, C1058 AAL, Cdad. Autónoma de Buenos Aires',
+    'google_maps': 'https://www.google.com/maps/place/On+Tap+Retiro/@-34.5968295,-58.3788677,20z/data=!4m17!1m10!3m9!1s0x95bccaa37c9450ad:0x9cdf7e292ed4c101!2sAlvear+Palace+Hotel!5m2!4m1!1i2!8m2!3d-34.5875836!4d-58.3888465!16s%2Fm%2F04n6dms!3m5!1s0x95bccab6218b0499:0xe616737bbfbf1fd0!8m2!3d-34.5965113!4d-58.3789635!16s%2Fg%2F11c6s4qd_0?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D'
+},
+{
+    'name': "Backroom Bar",
+    'type': 'bar',
+    'budget': 'moderate',
+    'distance': 'short_ride',
+    'vibe': 'chill',
+    'reservation': 'yes',
+    'group_size': ['solo','couple', 'small_group'],
+    'wifi': 'no',
+    'address': 'Jorge Luis Borges 1975, C1414 Cdad. Autónoma de Buenos Aires',
+    'google_maps': 'https://www.google.com/maps/place/Backroom+Bar/@-34.5863696,-58.4287946,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcb598cad115e7:0xbf2442650dc7236f!8m2!3d-34.586374!4d-58.426225!16s%2Fg%2F11n00pkpfn?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D'
+}, 
+{
+    'name': "Avant Garten",
+    'type': 'bar',
+    'budget': 'cheap',
+    'distance': 'short_ride',
+    'vibe': 'lively',
+    'reservation': 'no',
+    'group_size': ['solo','couple', 'small_group', 'large_group'],
+    'wifi': 'no',
+    'address': 'Av. del Libertador 3883, C1425 Cdad. Autónoma de Buenos Aires',
+    'google_maps': 'https://www.google.com/maps/place/Avant+Garten/@-34.5703033,-58.4214908,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcb59f2f6a142f:0x75b20a9cfd04e44e!8m2!3d-34.5703033!4d-58.4214908!16s%2Fg%2F11b_3gz0rq?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D'
+}
